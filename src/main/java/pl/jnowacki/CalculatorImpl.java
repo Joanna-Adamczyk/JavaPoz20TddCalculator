e01b44103c790442b5716fe6797b459f2245b9e0
Joanna Adamczyk <adamczyk.joanna94@gmail.com>
2019-10-26 12:56:58 +0200

    no message

---
+++ src/main/java/pl/jnowacki/CalculatorImpl.java
@@ -0,0 +1,38 @@
+package pl.jnowacki;
+
+public class CalculatorImpl implements Calculator {
+    @Override
+    public void pressNumber(Object number) {
+
+    }
+
+    @Override
+    public String display() {
+        return null;
+    }
+
+    @Override
+    public void add(int numberA, int numberB) {
+
+    }
+
+    @Override
+    public void sub(int numberA, int numberB) {
+
+    }
+
+    @Override
+    public void div(int numberA, int numberB) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void multi(int numberA, int numberB) {
+
+    }
+}
