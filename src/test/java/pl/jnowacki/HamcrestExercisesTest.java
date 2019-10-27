ae88653f6d3a6dbaf8d5551cb10a4ee6ad3cbcbc
Joanna Adamczyk <adamczyk.joanna94@gmail.com>
2019-10-27 14:59:41 +0100

    Hamcrest - feature matcher

--- src/test/java/pl/jnowacki/HamcrestExercisesTest.java
+++ src/test/java/pl/jnowacki/HamcrestExercisesTest.java
@@ -3,8 +3,14 @@ package pl.jnowacki;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static pl.jnowacki.GivenLetterItemCountListMatcher.hasGivenLetterCountItems;
+import static pl.jnowacki.PersonSchoolAgeMatcher.isEligibleToSchool;
 
 public class HamcrestExercisesTest {
 
@@ -19,29 +25,37 @@ public class HamcrestExercisesTest {
     }
 
     @Test
-    public void checkingString() {
+    public void doesStringContainKonfabulacja() {
+
         String value = "afdsfdsfdsfaqwtjjfvvjfyu";
+
         assertThat(value, not(containsString("konfabulacja")));
     }
 
     @Test
-    public void matrixCheckingValues() {
-        Integer[] array = {5,7,2};
-        assertThat(array, arrayContaining(5,7,2));
+    public void doesArrayContainRightValues() {
+
+        Integer[] values = {5, 7, 2};
+
+        assertThat(values, arrayContaining(5, 7, 2));
     }
 
     @Test
-    public void matrixCheckingValuesRandomOrder() {
-        Integer[] array = {5,7,2};
-        assertThat(array, arrayContainingInAnyOrder(5,7,2));
+    public void doesArrayContainRightValuesInAnyOrder() {
+
+        Integer[] values = {5, 7, 2};
+
+        assertThat(values, arrayContainingInAnyOrder(2, 5, 7));
     }
 
     @Test
-    public void matrix3() {
-        String[] list = {"raz", "dwa", "trzy"};
-        assertThat(list, arrayWithSize(3));
-        assertThat(list, hasItemInArray("dwa"));
-        }
+    public void doesListContainAndHasRightLength() {
+
+        List<String> values = Arrays.asList("raz", "dwa", "trzy");
+
+        assertThat(values, hasItem("dwa"));
+        assertThat(values, hasSize(3));
+    }
 
     @Test
     public void shouldBeginWithA() {
@@ -57,5 +71,31 @@ public class HamcrestExercisesTest {
                 return actual.charAt(0);
             }
         };
+    }
+
+    @Test
+    public void testIfHasOnly3LetterItems() {
+        List<String> listUnderTest = Arrays.asList("one", "two");
+
+        assertThat(listUnderTest, hasGivenLetterCountItems(3));
+    }
+
+    @Test
+    public void testIfHasOnly4LetterItems() {
+        List<String> listUnderTest = Arrays.asList("onea", "twoa");
+
+        assertThat(listUnderTest, hasGivenLetterCountItems(4));
+    }
+
+    @Test
+    public void testPersonAge() {
+
+        Person person1 = new Person(0);
+        Person person2 = new Person(6);
+        Person person3 = new Person(12);
+
+        assertThat(person1, not(isEligibleToSchool()));
+        assertThat(person2, not(isEligibleToSchool()));
+        assertThat(person3, isEligibleToSchool());
     }
 }
--- src/test/java/pl/jnowacki/NumberWrapper.java
+++ src/test/java/pl/jnowacki/NumberWrapper.java
@@ -2,23 +2,12 @@ package pl.jnowacki;
 
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
-import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-
-//feature matcher
 public class NumberWrapper {
-    public int getNumber() {
+    public int getNumber(){
         return 15;
     }
 
-    @Test
-    public void shouldReturnRightNumber() {
-        NumberWrapper wrapper = new NumberWrapper();
-        assertThat(wrapper, returnsRightNumber(is(15)));
-    }
     public static Matcher<NumberWrapper> returnsRightNumber(Matcher<? super Integer> matcher) {
         return new FeatureMatcher<NumberWrapper, Integer>(matcher, "a wrapper that returns", "returned value") {
             @Override
@@ -28,4 +17,3 @@ public class NumberWrapper {
         };
     }
 }
-
